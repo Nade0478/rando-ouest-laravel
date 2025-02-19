910d8e1cd5ce37c30e64a2d6f0f521eafe0a552e
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-02-19 17:06:56 +0100

    modif table

---
+++ database/migrations/2025_02_19_151430_create_opinion_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('opinion', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_opinion');
+            $table->text('content_opinion');
+            $table->integer('note_opinion');
+            $table->unsignedBigInteger('users_id');
+            $table->unsignedBigInteger('place_rando_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('opinion');
+    }
+};

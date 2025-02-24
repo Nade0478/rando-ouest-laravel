e38c8cd65c6b386a109696f740743b885f74d179
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-02-24 18:40:59 +0100

    modif table pivot

--- database/migrations/07_create_manage_places_table.php
+++ database/migrations/07_create_manage_places_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Place_rando;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,10 +14,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('manage_places', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('users_id');
-            $table->unsignedBigInteger('place_rando_id');
-            $table->timestamps();
+            $table->primary(['user_id', 'place_rando_id']);
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Place_rando::class);
         });
     }
 
